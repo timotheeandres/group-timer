35cdea58f8d0913ad114de26ac129d12054f60e9
Timothée Andres <andres.timothee@gmail.com>
2025-05-16 17:30:52 +0200

    Install ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+  { files: [ "**/*.{js,mjs,cjs,ts}" ], plugins: { js }, extends: [ "js/recommended" ] },
+  { files: [ "**/*.{js,mjs,cjs,ts}" ], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  {
+    rules: {
+      semi: [ "warn", "always" ],
+      "no-unused-vars": [ "error", {
+        varsIgnorePattern: "^_$",
+        argsIgnorePattern: "^_$",
+        destructuredArrayIgnorePattern: "^_$"
+      } ],
+      "array-bracket-newline": [ "warn", "consistent" ],
+      "array-element-newline": [ "warn", "consistent" ],
+      "object-curly-spacing": [ "warn", "always" ],
+      "array-bracket-spacing": [ "warn", "always" ],
+    }
+  }
+]);
