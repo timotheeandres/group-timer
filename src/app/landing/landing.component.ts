0af95de89ce4f9b8e4aceff2288a5435e251081c
Timothée Andres <andres.timothee@gmail.com>
2025-05-17 08:25:19 +0200

    Add validation on form

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -4,7 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatTimepickerModule } from '@angular/material/timepicker';
 import { Router } from '@angular/router';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { constructNow, formatISO, roundToNearestMinutes } from 'date-fns';
 
 @Component({
@@ -20,12 +20,18 @@ import { constructNow, formatISO, roundToNearestMinutes } from 'date-fns';
   styleUrl: './landing.component.css'
 })
 export class LandingComponent {
+  protected readonly minGroups = 1;
+  protected readonly maxGroups = 15;
+
   protected router = inject(Router);
 
   protected readonly formGroup = new FormGroup(
     {
-      nbGroups: new FormControl(5),
-      deadline: new FormControl(roundToNearestMinutes(constructNow(undefined), { nearestTo: 15, roundingMethod: "ceil" }))
+      nbGroups: new FormControl(5, { validators: [ Validators.required, Validators.min(this.minGroups), Validators.max(this.maxGroups) ] }),
+      deadline: new FormControl(roundToNearestMinutes(constructNow(undefined), {
+        nearestTo: 15,
+        roundingMethod: "ceil"
+      }), { validators: [ Validators.required ] })
     }
   );
 
